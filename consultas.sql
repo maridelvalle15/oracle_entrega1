9f0688a781b2631750d23fb3cf7cd1679e8c9925
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-08-14 15:56:51 -0430

    Consultas actualizadas de acuerdo a nuevo esquema

--- consultas.sql
+++ consultas.sql
@@ -2,24 +2,45 @@
 ************** INSERCIONES ***************
 *****************************************/
 
-/* Creamos jefes */
+/*		Creamos jefes		*/
 INSERT INTO Jefe VALUES
 	('6544230','Carlos','Rodriguez','04123212287',NULL);
 INSERT INTO Jefe VALUES
 	('18765234','Maria','Gomez','04243467891',NULL);
 
-/* Creamos departamentos, con referencia al jefe que los dirige */
+/*		Creamos departamentos, con referencia a:
+ 		- jefe que los dirige  
+ 		- tabla de referencias de desarrolladores
+ */
+INSERT INTO Departamento
+	SELECT 'Departamento 1', 
+			REF(j),
+			Desarrollador_ref(REF(d))
+	FROM Jefe j,Desarrollador d WHERE j.cedula = '6544230' and d.cedula='20674321';
+
 INSERT INTO Departamento
 	SELECT 'Departamento 1', REF(j)
 	FROM Jefe j WHERE j.cedula = '6544230';
+
 INSERT INTO Departamento
 	SELECT 'Departamento 2', REF(j)
 	FROM Jefe j WHERE j.cedula = '6544230';
-/* Creamos desarrolladores, con referencia al departamento en el cual trabajan */
+
+
+INSERT INTO Desarrollador
+	VALUES (Desarrollador_senior_T('20674321','Mathieu','De Valery','04143276648',100000,20));
+
+
+/*		Creamos desarrolladores, con referencia a:
+		- departamento en el cual trabajan
+*/
 INSERT INTO Desarrollador
 	SELECT Desarrollador_movil_T('20674321','Mathieu','De Valery','04143276648',REF(d),100000,20)
 	FROM Departamento d WHERE d.nombre='Departamento 1';
 
+
+UPDATE Jefe SET dep = (SELECT REF(d) FROM Departamento d WHERE d.nombre='Departamento 1') WHERE cedula='6544230';
+UPDATE Jefe SET dep = (SELECT REF(d) FROM Departamento d WHERE d.nombre='Departamento 2') WHERE cedula='6544230';
 
 /* UPDATE STATEMENT
 UPDATE Departamento SET jefe_dep = (SELECT REF(j) FROM Jefe j WHERE j.cedula='6544230') WHERE nombre='Departamento 1';
